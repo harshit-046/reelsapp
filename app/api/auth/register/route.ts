432b02e2b97a6bcdf48423a9320bd30d973fd2ca
harshit51046 <harshitkatiyar817@gmail.com>
2025-03-18 21:47:00 +0000

    authenciation

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,43 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { NextRequest,NextResponse } from "next/server";
+
+export async function POST(request: NextRequest){
+    try{
+        const {email , password} = await request.json();
+        if(!email || !password){
+            return NextResponse.json(
+                {error: "Email and password are required"},
+                {status: 400}
+            );
+        }
+
+        await connectToDatabase();
+
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            return NextResponse.json(
+                {error: "User already exists"},
+                {status: 400}
+            );
+        }
+
+        await User.create({
+            email,
+            password
+        })
+
+        return NextResponse.json(
+            {message: "Registered Successfully!!!"},
+            {status: 201}
+        )
+    }
+    catch(error){
+        return NextResponse.json(
+            {error: "Failed to Register User"},
+            {status: 500}
+        )
+    }
+}
+
